1e87ba7da5598f75c940cd1d0dde839f838ac991
Aaron Thompson <aarontomo@protonmail.com>
2021-06-29 18:24:33 +0100

    This commit alligns the UI and alters the UI. If statement added for when a text box is left blank, value now defaults to the colour white.

--- myscripts.js
+++ myscripts.js
@@ -2,12 +2,18 @@ console.log("Hello World")
 
 function Button(){
 
-    var inputColour1
-    var inputColour2 
+    var inputColour1 = ""
+    var inputColour2  = ""
 
     inputColour1 = document.getElementById("firstColour").value
     inputColour2 = document.getElementById("secondColour").value
 
+    if(inputColour1.length === 0){
+        inputColour1 = "white"
+    }
+    if(inputColour2.length === 0){
+        inputColour2 = "white"
+    }
     console.log(inputColour1)
     console.log(inputColour2)
 
